546b0d5719a99642ff3d6403b6eb7b8a059e36e5
joshb4u <59143348+joshb4u@users.noreply.github.com>
2020-06-11 17:58:51 -0400

    Updated Workout.js file

--- models/Workout.js
+++ models/Workout.js
@@ -1,1 +1,52 @@
-// TO be coded
+const mongoose = require('mongoose')
+const dB = mongoose.dB
+const workoutdB = new dB(
+    {
+        day:{
+            type: Date,
+            default: Date.now
+        },
+        exercises:[
+            {
+                category:{
+                    type: String,
+                    trim: true,
+                    required:'Exercise Category Required'
+                },
+                name:{
+                    type: String,
+                    trim: true,
+                    required: 'Exercise Name Required'
+                },
+                duration:{
+                    type: Number,
+                    required: 'Exercise Duration Required'
+                },
+                weight:{
+                    type: Number
+                },
+                reps:{
+                    type: Number
+                },
+                sets:{
+                    type: Number
+                },
+                distance:{
+                    type: Number
+                }
+            }
+        ]
+    },
+    {
+        toJSON:{
+            virtuals: true
+        }
+    }
+)
+workoutdB.virtual('totalDuration').get(function(){
+    return this.exercises.reduce((total, exercise)=>{
+        return total+exercise.duration
+    },0)
+})
+const Workout = mongoose.model('Workout', workoutdB)
+module.exports = Workout
