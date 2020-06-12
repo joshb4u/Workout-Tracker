7f4fbeb71a27f2d38874167b973237bd5ab3a9fe
joshb4u <59143348+joshb4u@users.noreply.github.com>
2020-06-11 22:37:21 -0400

    Added html-routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const path = require('path')
+
+router.get('/exercise', function(req, res){
+    res.sendFile(path.join(_dirname,'../public/exercise.html'))
+})
+
+router.get('/', function(req, res){
+    res.sendFile(path.join(_dirname,'../public/index.html'))
+})
+
+router.get('/stats', function(req, res){
+    res.sendFile(path.join(_dirname,'../public/stats.html'))
+})
+
+module.exports = router
