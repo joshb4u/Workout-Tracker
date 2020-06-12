cd3af6de867d5d70607435c641a1f0d710b0b8a8
joshb4u <59143348+joshb4u@users.noreply.github.com>
2020-06-11 22:57:55 -0400

    Added server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-// To be coded
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',{
+    useNewUrlParser: true,
+    useFindandModify: false
+})
+
+app.use(require('./routes/apiRoutes'))
+app.use(require('./routes/htmlRoutes'))
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`)
+})
